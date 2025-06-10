3b7a8e84a45ab45405835ba25f00da27a5fbd385
ashishrao9983 <ashishrao9983@gmail.com>
2025-06-10 12:31:06 +0530

    changes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+        
+      
+        <div className="relative z-10 max-w-screen-xl px-4 pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between">
+          
+        
+          <div className="sm:w-1/2 flex justify-center mb-10 sm:mb-0">
+            <img
+              className="w-72 sm:w-96"
+              src="https://gofloaters.com/work-from-anywhere-toolkit/assets/images/remote2.png"
+              alt="image1"
+            />
+          </div>
+
+         
+          <div className="sm:w-1/2 text-center sm:text-right">
+            
+           
+            <h2 className="text-4xl font-bold sm:text-5xl text-gray-900">
+              Download Now
+            </h2>
+            <p className="text-xl mt-2 font-bold text-gray-700">Lorem Ipsum</p>
+
+
+            <Link
+              className="inline-flex mt-6 items-center px-6 py-3 font-medium bg-orange-700 text-white rounded-lg hover:opacity-80 transition"
+              to="/"
+            >
+              <svg
+                fill="white"
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              >
+                <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
+              </svg>
+              &nbsp; Download now
+            </Link>
+          </div>
+        </div>
+      </aside>
+
+      {/* Unchanged bottom image section */}
+      <div className="grid place-items-center sm:mt-20">
+        <img
+          className="sm:w-96 w-48"
+          src="https://globalvoices.org/wp-content/uploads/2021/05/Remote1-1-800x465.png"
+          alt="image2"
+        />
+      </div>
+
+      {/* Unchanged heading below */}
+      <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
+        Lorem Ipsum Yojo
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
