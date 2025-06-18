e7b073df6695acc4b67473d92367f107ab4ea6da
ashishrao9983 <ashishrao9983@gmail.com>
2025-06-18 12:49:36 +0530

    dubbging

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Home-Page-Handling/',
+   base: '/homepage-roting-react/',
    plugins: 
   [ tailwindcss(),
     react()],
